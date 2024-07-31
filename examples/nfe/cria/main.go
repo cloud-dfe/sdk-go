537040cd03164c2ca31fdf90c6d15ffd2651e740
Gabriel Arantd Felipe <126432892+G-Arantd@users.noreply.github.com>
2024-07-31 15:29:12 -0300

    Update main.go

--- examples/nfe/cria/main.go
+++ examples/nfe/cria/main.go
@@ -20,7 +20,64 @@ func main() {
 
 	nfe := sdk_cloud_dfe.Nfe(config)
 
-	listaItens := map[string]interface{}{}
+	listaItens := []map[string]interface{}{
+		{
+			"numero_item":               "1",
+			"codigo_produto":            "000297",
+			"descricao":                 "SAL GROSSO 50KGS",
+			"codigo_ncm":                "84159020",
+			"cfop":                      "5102",
+			"unidade_comercial":         "SC",
+			"quantidade_comercial":      10,
+			"valor_unitario_comercial":  "22.45",
+			"valor_bruto":               "224.50",
+			"unidade_tributavel":        "SC",
+			"quantidade_tributavel":     "10.00",
+			"valor_unitario_tributavel": "22.45",
+			"origem":                    "0",
+			"inclui_no_total":           "1",
+			"imposto": map[string]interface{}{
+				"valor_aproximado_tributos": 9.43,
+				"icms": map[string]interface{}{
+					"situacao_tributaria":                 "102",
+					"aliquota_credito_simples":            "0",
+					"valor_credito_simples":               "0",
+					"modalidade_base_calculo":             "3",
+					"valor_base_calculo":                  "0.00",
+					"modalidade_base_calculo_st":          "4",
+					"aliquota_reducao_base_calculo":       "0.00",
+					"aliquota":                            "0.00",
+					"aliquota_final":                      "0.00",
+					"valor":                               "0.00",
+					"aliquota_margem_valor_adicionado_st": "0.00",
+					"aliquota_reducao_base_calculo_st":    "0.00",
+					"valor_base_calculo_st":               "0.00",
+					"aliquota_st":                         "0.00",
+					"valor_st":                            "0.00",
+				},
+				"fcp": map[string]interface{}{
+					"aliquota": "1.65",
+				},
+				"pis": map[string]interface{}{
+					"situacao_tributaria": "01",
+					"valor_base_calculo":  224.5,
+					"aliquota":            "1.65",
+					"valor":               "3.70",
+				},
+				"cofins": map[string]interface{}{
+					"situacao_tributaria": "01",
+					"valor_base_calculo":  224.5,
+					"aliquota":            "7.60",
+					"valor":               "17.06",
+				},
+			},
+			"valor_desconto":              0,
+			"valor_frete":                 0,
+			"valor_seguro":                0,
+			"valor_outras_despesas":       0,
+			"informacoes_adicionais": "Valor aproximado tributos R$: 9,43 (4,20%) Fonte: IBPT",
+		},
+	}
 
 	payload := map[string]interface{}{
 		"natureza_operacao":  "VENDA DENTRO DO ESTADO",
